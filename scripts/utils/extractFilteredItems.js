1ad392cbebb9025dbd5716d5dd00d4847050619b
elistoj <atanastojaneli@gmail.com>
2024-03-18 12:34:28 +0100

    Create extractFilteredItems.js

---
+++ scripts/utils/extractFilteredItems.js
@@ -0,0 +1,15 @@
+import { Normalization } from "./normalization.js";
+
+export const extractFilteredItems = filteredRecipes => {
+    const filteredItems = [];
+
+    filteredRecipes.forEach(recipe => {
+        filteredItems.push(Normalization(recipe.appliance));
+
+        recipe.ustensils.forEach(ustensil => filteredItems.push(Normalization(ustensil)));
+
+        recipe.ingredients.forEach(ingredient => filteredItems.push(Normalization(ingredient.ingredient)));
+    });
+
+    return filteredItems;
+};

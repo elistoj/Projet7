2f64a2b3fc981b113ac5bf832759517f70289b8d
elistoj <atanastojaneli@gmail.com>
2024-03-18 12:34:17 +0100

    Update index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,9 @@
 import CardRecipe from '../cards/CardRecipe.js'; // Importe la classe CardRecipe pour créer les cartes de recettes
 import { fetchRecipes } from '../utils/fetchRecipes.js'; // Importe la fonction fetchRecipes pour récupérer les recettes
 import { performSearch } from '../utils/performSearch.js'; // Importe la fonction performSearch pour effectuer une recherche
+import Dropdown from '../utils/Dropdown.js';
+import {openCloseDropdown} from '../utils/dropdownEvent.js';
+import {extractUniqueProperties} from '../utils/extractUniqueProperties.js';
 
 // Récupère toutes les recettes via la fonction fetchRecipes et les stocke dans la variable allRecipes
 export const allRecipes = await fetchRecipes();
@@ -16,6 +19,21 @@ export const dropdowns = [];
 
 // Récupère l'élément HTML correspondant à la zone de recherche de recettes
 export const searchInput = document.querySelector('#search-recipe');
+
+
+const displayDropdownSection = () => {
+
+    const numberOfRecipes = document.querySelector('.recipes_count');
+    numberOfRecipes.textContent = `${allRecipes.length} recettes`;
+
+    dropdowns.push(new Dropdown('Ingrédients', extractUniqueProperties(allRecipes).ingredients));
+    dropdowns.push(new Dropdown('Appareils', extractUniqueProperties(allRecipes).appliances));
+    dropdowns.push(new Dropdown('Ustensiles', extractUniqueProperties(allRecipes).ustensils));
+
+    const filterSection = document.querySelector('.filter_section');
+    dropdowns.forEach(dropdown => filterSection.insertBefore(dropdown.createDropdown(), numberOfRecipes));
+};
+
 
 // Fonction asynchrone pour afficher les cartes de recettes
 export const displayRecipesCards = async () => {
@@ -36,3 +54,7 @@ displayRecipesCards();
 
 // Appelle la fonction pour exécuter la recherche
 performSearch();
+
+displayDropdownSection ();
+
+openCloseDropdown();

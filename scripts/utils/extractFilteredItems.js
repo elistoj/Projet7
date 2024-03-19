14571acaea4955ad304b21e0d812438dcbe8f423
elistoj <atanastojaneli@gmail.com>
2024-03-19 18:51:16 +0100

    Update extractFilteredItems.js

--- scripts/utils/extractFilteredItems.js
+++ scripts/utils/extractFilteredItems.js
@@ -1,19 +1,15 @@
-// Cette fonction `extractFilteredItems` prend en entrée un tableau d'objets représentant des recettes filtrées et retourne un tableau contenant tous les éléments filtrés normalisés.
+import { Normalization } from "../utils/normalization.js";
 
-// La fonction commence par initialiser un tableau vide pour stocker les éléments filtrés.
-const filteredItems = [];
+export const extractFilteredItems = filteredRecipes => {
+    const filteredItems = [];
 
-// Ensuite, elle itère à travers chaque recette dans le tableau des recettes filtrées.
-filteredRecipes.forEach(recipe => {
-    // Pour chaque recette, elle normalise le nom de l'appareil associé à la recette en utilisant la fonction Normalization et l'ajoute au tableau des éléments filtrés.
-    filteredItems.push(Normalization(recipe.appliance));
+    filteredRecipes.forEach(recipe => {
+        filteredItems.push(Normalization(recipe.appliance));
 
-    // Ensuite, elle itère à travers chaque ustensile de la recette.
-    recipe.ustensils.forEach(ustensil => filteredItems.push(Normalization(ustensil)));
+        recipe.ustensils.forEach(ustensil => filteredItems.push(Normalization(ustensil)));
 
-    // Enfin, elle itère à travers chaque ingrédient de la recette.
-    recipe.ingredients.forEach(ingredient => filteredItems.push(Normalization(ingredient.ingredient)));
-});
+        recipe.ingredients.forEach(ingredient => filteredItems.push(Normalization(ingredient.ingredient)));
+    });
 
-// Une fois toutes les recettes parcourues, la fonction retourne le tableau contenant tous les éléments filtrés normalisés.
-return filteredItems;
+    return filteredItems;
+};

c9b308b2446753081ea6cb0101314f84a75d63ba
elistoj <atanastojaneli@gmail.com>
2024-03-19 12:40:08 +0100

    Update extractUniqueProperties.js

--- scripts/utils/extractUniqueProperties.js
+++ scripts/utils/extractUniqueProperties.js
@@ -1,3 +1,7 @@
+// Cette fonction `extractUniqueProperties` prend en entrée un tableau d'objets représentant des recettes et retourne un objet contenant les propriétés uniques (ingrédients, appareils, ustensiles) de ces recettes.
+
+// La fonction initialise un objet `uniqueProperties` contenant trois ensembles (Set) vides pour stocker les valeurs uniques des ingrédients, des appareils et des ustensiles.
+
 export const extractUniqueProperties = recipes => {
     const uniqueProperties = {
         ingredients: new Set(),

eefbdff9e17dd7e73ab30606e47cb8d6d83a081a
elistoj <atanastojaneli@gmail.com>
2024-03-19 19:10:05 +0100

    Update filterRecipesByText.js

--- scripts/utils/filterRecipesByText.js
+++ scripts/utils/filterRecipesByText.js
@@ -4,33 +4,24 @@ import { Normalization } from "./normalization.js";
 import { updateWithFilteredRecipes } from "./updateWithFilteredRecipes.js";
 
 // Fonction pour filtrer les recettes en fonction du texte saisi
-export const filterRecipesByText = (recipes, inputValue) => {
-    // Normalisation des valeurs d'entrée
+ export const filterRecipesByText = (recipes, inputValue) => {
+    // Normalisation de la valeur d'entrée
     const normalizedInputValue = Normalization(inputValue);
 
-// Filtrer les recettes en fonction du texte saisi
-    const filteredRecipes = [];
-    for (let i = 0; i < recipes.length; i++) {
-        const recipe = recipes[i];
+    // Filtrage des recettes en fonction du texte saisi
+    const filteredRecipes = recipes.filter(recipe => {
         const { description, ingredients, name } = recipe;
 
-        if (
-            Normalization(description).includes(normalizedInputValue) || // Vérifie si la description contient du texte
-            ingredients.some(ingredient => Normalization(ingredient.ingredient).includes(normalizedInputValue)) || // Vérifie si un ingrédient contient du texte saisi
+        return (
+            Normalization(description).includes(normalizedInputValue) || // Vérifie si la description contient le texte saisi
+            ingredients.some(ingredient => Normalization(ingredient.ingredient).includes(normalizedInputValue)) || // Vérifie si un ingrédient contient le texte saisi
             Normalization(name).includes(normalizedInputValue) // Vérifie si le nom de la recette contient le texte saisi
-        ) {
-            filteredRecipes.push(recipe);
-        }
-    }
+        );
+    });
 
-// Met à jour les recettes actuelles avec des recettes filtrées par texte
+    // Met à jour les recettes actuelles avec les recettes filtrées par texte
     updateCurrentRecipes(filteredRecipes);
 
-// Met à jour la vue avec les recettes filtrées par texte
+    // Met à jour l'affichage avec les recettes filtrées par texte
     updateWithFilteredRecipes(filteredRecipes);
 };
-
-//Ce code définit une fonction filterRecipesByText qui prend en entrée une liste de recettes 
-//et une valeur de recherche textuelle. 
-//Il filtre ensuite les recettes en fonction de cette valeur de recherche, en cherchant dans la description, les ingrédients et le nom de chaque recette. 
-//Enfin, il met à jour l'affichage avec les recettes filtrées en appelant les fonctions updateCurrentRecipes et updateWithFilteredRecipes.

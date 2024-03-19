abe120fffdd36a360d15cc1341eada8abe4c8f40
elistoj <atanastojaneli@gmail.com>
2024-03-19 18:51:20 +0100

    Update updateWithFilteredRecipes.js

--- scripts/utils/updateWithFilteredRecipes.js
+++ scripts/utils/updateWithFilteredRecipes.js
@@ -1,34 +1,32 @@
 // Import des classes nécessaires depuis d'autres fichiers
 import CardRecipe from "../cards/CardRecipe.js";
 import Recipe from "../cards/Recipe.js";
+import { extractFilteredItems } from "../utils/extractFilteredItems.js";
+import { dropdowns } from "../pages/index.js";
 
-// Fonction pour mettre à jour l'affichage avec les recettes filtrées
 export const updateWithFilteredRecipes = filteredRecipes => {
-    // Sélection de la section des cartes de recette dans le DOM
+
     const cardSection = document.querySelector('.card_section');
-    // Sélection de l'élément affichant le nombre de recettes trouvées dans le DOM
     const numberOfRecipes = document.querySelector('.recipes_count');
     
-    // Si aucune recette n'est trouvée, afficher un message approprié
     if (!filteredRecipes.length) {
         cardSection.innerHTML = "<p>Aucune recette n'a été trouvée.</p>";
         numberOfRecipes.textContent = ``;
     } else {
-        // Sinon, afficher les recettes filtrées
-        cardSection.innerHTML = ""; // Efface tout contenu précédent dans la section des cartes de recette
-
-        // Met à jour le nombre de recettes affichées
+        cardSection.innerHTML = "";
         numberOfRecipes.textContent = `${filteredRecipes.length} ${filteredRecipes.length === 1 ? 'recette' : 'recettes'}`;
 
-        // Pour chaque recette filtrée, créer une carte de recette et l'ajouter à la section des cartes de recette
         filteredRecipes
-            .map(recipe => new Recipe(recipe)) // Création d'une instance de la classe Recipe pour chaque recette
+            .map(recipe => new Recipe(recipe))
             .forEach(recipe => {
-                const templateCard = new CardRecipe(recipe); // Création d'une carte de recette à partir de l'instance de Recipe
-                templateCard.createCard(); // Ajout de la carte à la section des cartes de recette dans le DOM
+                const templateCard = new CardRecipe(recipe);
+                templateCard.createCard();
             });
-    }
-  
+    };
+
+    const filteredItems = extractFilteredItems(filteredRecipes);
+
+    dropdowns.forEach(dropdown => dropdown.updateItems(filteredItems));
 
 };
 //Cette fonction updateWithFilteredRecipes met à jour l'affichage des recettes dans la section

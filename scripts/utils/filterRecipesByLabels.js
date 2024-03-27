fab07d8e353cccefc8ca7105cd32e9ea1b1b8dac
elistoj <atanastojaneli@gmail.com>
2024-03-27 19:14:30 +0100

    Update filterRecipesByLabels.js

--- scripts/utils/filterRecipesByLabels.js
+++ scripts/utils/filterRecipesByLabels.js
@@ -2,16 +2,16 @@
 import { UpdateFilteredRecipes, updateCurrentRecipes } from "./UpdateRecipes.js";
 import { Normalization } from "./normalization.js";
 
-// Déclaration d'un tableau pour stocker les recettes filtrées par étiquettes
+// Déclaration d'un tableau pour stocker les recettes filtrées par tags
 export let recipesFilteredByLabels = [];
 
-// Fonction pour filtrer les recettes en fonction des étiquettes
+// Fonction pour filtrer les recettes en fonction des tags
 export const filterRecipesByLabels = (recipes, tags) => {
-    // Filtrage des recettes en fonction des étiquettes
-    const filteredRecipes = recipes.filter(recipe => {
+    // Filtrage des recettes en fonction des tags
+    const filteredRecipesByTags = recipes.filter(recipe => {
         const { appliance, ustensils, ingredients } = recipe;
 
-        // Normalisation des étiquettes
+        // Normalisation des tags
         const normalizedTags = tags.map(tag => Normalization(tag));
 
         // Vérifie que tous les tags sont présents dans la recette
@@ -24,17 +24,16 @@ export const filterRecipesByLabels = (recipes, tags) => {
         );
     });
 
-    // Met à jour le tableau des recettes filtrées par étiquettes
-    recipesFilteredByLabels = filteredRecipes;
+    // Met à jour le tableau des recettes filtrées par tags
+    recipesFilteredByLabels = filteredRecipesByTags;
     
     // Met à jour les recettes actuelles avec les recettes filtrées
-    updateCurrentRecipes(filteredRecipes);
+    updateCurrentRecipes(filteredRecipesByTags);
 
     // Met à jour l'affichage avec les recettes filtrées
-    UpdateFilteredRecipes(filteredRecipes);
+    UpdateFilteredRecipes(filteredRecipesByTags);
 }; 
 //Ce code définit une fonction filterRecipesByLabels qui prend en entrée une liste de recettes et des étiquettes de filtre. Il filtre ensuite les recettes
 // en fonction de ces étiquettes et met à jour le tableau recipesFilteredByLabels avec les recettes filtrées. 
 //Enfin, il met à jour l'affichage avec les recettes filtrées en appelant les fonctions updateCurrentRecipes et UpdateFilteredRecipes.
-
 

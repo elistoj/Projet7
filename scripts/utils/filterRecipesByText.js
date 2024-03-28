7caa52a3adc7d302eeb3780dba7f063361bdbeca
elistoj <90917944+elistoj@users.noreply.github.com>
2024-03-28 12:38:54 +0100

    Update filterRecipesByText.js

--- scripts/utils/filterRecipesByText.js
+++ scripts/utils/filterRecipesByText.js
@@ -1,27 +1,30 @@
 // Import des fonctions nécessaires depuis d'autres fichiers
-import { updateCurrentRecipes } from "./UpdateRecipes.js";
+import { updateCurrentRecipes } from "./UpdateCurrentRecipes.js";
 import { Normalization } from "./normalization.js";
-import { UpdateFilteredRecipes } from "./UpdateRecipes.js";
+import { updateWithFilteredRecipes } from "./updateWithFilteredRecipes.js";
 
-// Fonction pour filtrer les recettes en fonction du texte saisi
- export const filterRecipesByText = (recipes, inputValue) => {
-    // Normalisation de la valeur d'entrée
+export const filterRecipesByText = (recipes, inputValue) => {
+// Normalisation de la valeur d'entrée
     const normalizedInputValue = Normalization(inputValue);
 
     // Filtrage des recettes en fonction du texte saisi
-    const filteredRecipesByInput = recipes.filter(recipe => {
+    const filteredRecipes = [];
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
         const { description, ingredients, name } = recipe;
 
-        return (
+        if (
             Normalization(description).includes(normalizedInputValue) || // Vérifie si la description contient le texte saisi
-            ingredients.some(ingredient => Normalization(ingredient.ingredient).includes(normalizedInputValue)) || // Vérifie si un ingrédient contient le texte saisi
-            Normalization(name).includes(normalizedInputValue) // Vérifie si le nom de la recette contient le texte saisi
-        );
-    });
+            ingredients.some(ingredient => Normalization(ingredient.ingredient).includes(normalizedInputValue)) || // Vérifie si un ingrédient contient du texte saisi
+            Normalization(name).includes(normalizedInputValue) // Vérifie si le nom de la recette contient du texte saisi
+        ) {
+            filteredRecipes.push(recipe);
+        }
+    }
 
-    // Met à jour les recettes actuelles avec les recettes filtrées par texte
-    updateCurrentRecipes(filteredRecipesByInput);
+// Met à jour les recettes actuelles avec des recettes filtrées par texte
+    updateCurrentRecipes(filteredRecipes);
 
-    // Met à jour l'affichage avec les recettes filtrées par texte
-    UpdateFilteredRecipes(filteredRecipesByInput);
+// Met à jour la vue avec les recettes filtrées par texte
+    updateWithFilteredRecipes(filteredRecipes);
 };
